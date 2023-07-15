42eb9b2af54009a73b558ca46c25f7892a8735a4
Dmytro Kozhukhar <424523+dkozhukhar@users.noreply.github.com>
2023-07-15 19:29:43 +0300

    modify the code to work with a list of words

--- ziplm.py
+++ ziplm.py
@@ -1,9 +1,11 @@
 import gzip
 import bz2
 import lzma
-
+import urllib.request
+import re
 import numpy as np
 import scipy.special
+from collections import Counter
 
 class ZipModel:
     def __init__(self, vocabulary, training="", compressor=gzip, conversion=np.log(256)):
@@ -15,17 +17,17 @@ class ZipModel:
 
     def logprobs(self, prefix="", temperature=1):
         code_lengths = np.array([
-            len(self.compressor.compress("".join([self.training, prefix, v]).encode()))
+            len(self.compressor.compress(" ".join([self.training, prefix, v]).encode()))
             for v in self.vocabulary
         ])
         return scipy.special.log_softmax(-code_lengths*self.conversion*(1/temperature))
                                          
     def sequence_logprob(self, sequence, prefix="", temperature=1):
         score = 0.0
-        for x in sequence:
+        for x in sequence.split():
             scores = self.logprobs(prefix, temperature=temperature)
             score += scores[self.index[x]]
-            prefix += x
+            prefix += " " + x
         return score
 
     def sample(self, prefix="", temperature=1):
@@ -38,5 +40,5 @@ class ZipModel:
         for k in range(maxlen):
             result = self.sample(sequence, temperature=temperature)
             yield result
-            sequence += result
+            sequence += " " + result
 
